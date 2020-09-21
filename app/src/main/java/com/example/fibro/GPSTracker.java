8a69d8104685ff1190015cfd7b8fd74a87a7fa27
tuomakuh <70937274+tuomakuh@users.noreply.github.com>
2020-09-21 12:24:18 +0300

    Added gson-2.8.6

---
+++ app/src/main/java/com/example/fibro/GPSTracker.java
@@ -0,0 +1,120 @@
+package com.example.fibro;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import androidx.core.app.ActivityCompat;
+
+import java.security.Provider;
+
+public class GPSTracker extends Service implements LocationListener {
+    private final Context mContext;
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+
+    Location location;
+    double latitude;
+    double longitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context){
+        this.mContext = context;
+        getLocation();
+    }
+    public Location getLocation(){
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+            if(!isGPSEnabled && !isNetworkEnabled){
+
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled){
+                    if(ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions((Activity) mContext, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null){
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if(location != null){
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                if(isGPSEnabled){
+                    if(location == null){
+                        if(ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions((Activity) mContext, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+                        }
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,this);
+                        if (locationManager != null){
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                            if(location != null){
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return location;
+    }
+    public void stopUsingGPS(){
+        if(locationManager != null){
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/fibro/Weather.java
+++ app/src/main/java/com/example/fibro/Weather.java
@@ -1,5 +1,6 @@
 package com.example.fibro;
 
+import android.content.Context;
 import android.os.StrictMode;
 import android.util.Log;
 
@@ -21,8 +22,14 @@ import com.google.gson.reflect.*;
 public class Weather { // TODO: Run networking on new Thread or AsyncTask
 
     double pressure;
+    private double mLat;
+    private double mLon;
 
-    public Weather(){ // constructor
+
+
+    public Weather(double lat, double lon){ // constructor
+        mLat = lat;
+        mLon = lon;
     }
     /***
      * Converts JSON data into a Map using GSON
@@ -34,13 +41,19 @@ public class Weather { // TODO: Run networking on new Thread or AsyncTask
         return map;
     }
 
+    public void refresh(){
+
+    }
+
     /***
      * Retrieves / updates weather data from OpenWeatherMap API
-     * and stores the values in variables for later use
+     * and stores the values in variables for later use.
+     * Overrides run method from Thread to run networking in a thread other than main.
      */
+
     public void getData(){
 
-        String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + "Espoo" + "&appid=" + "71157a0f49ae0e66bc6ad5d523d8554b"; // API URL + location and API key TODO: variable location
+        String urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=" + mLat + "&lon=" + mLon + "&exclude=current,minutely,hourly" + "&appid=" + "71157a0f49ae0e66bc6ad5d523d8554b"; // API URL + location and API key TODO: variable location
 
         try{
             StringBuilder result = new StringBuilder();
@@ -54,15 +67,21 @@ public class Weather { // TODO: Run networking on new Thread or AsyncTask
             rd.close();
             Log.i("FIBRO", result.toString()); // Log raw data to Logcat
             Map<String, Object> respMap = jsonToMap(result.toString()); // Maps the raw data
-            Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString()); // Maps data from category "main" to its own Map
+            Map<String, Object> dayMap = jsonToMap(respMap.get("daily").toString()); // Maps data from category "main" to its own Map
+            Map<String, Object> day0Map = jsonToMap(dayMap.get("0").toString());
+            Map<String, Object> day1Map = jsonToMap(respMap.get("1").toString());
+            Map<String, Object> day2Map = jsonToMap(respMap.get("2").toString());
+            Map<String, Object> day3Map = jsonToMap(respMap.get("3").toString());
 
-            pressure = Double.parseDouble(mainMap.get("pressure").toString()); // Assign value "pressure" from "main" category of Mapped data to variable
+
+            pressure = Double.parseDouble(day0Map.get("pressure").toString()); // Assign value "pressure" from "main" category of Mapped data to variable
                                                                                 // TODO: Read and store more information into other variables
 
 
         } catch(IOException e){
             Log.e("FIBRO",e.getMessage()); // Log possible errors to Logcat
         }
+
     }
 
     /***
