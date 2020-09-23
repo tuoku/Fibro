0b5fc054b288fd5a39c412a3f87b54fc097434db
DDiyar <ddogru10@gmail.com>
2020-09-23 10:44:41 +0300

    Tein 50% main activitysta

--- app/src/main/java/com/example/fibro/MainActivity.java
+++ app/src/main/java/com/example/fibro/MainActivity.java
@@ -7,30 +7,19 @@ import android.os.StrictMode;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-TextView tv;
-TextView lat;
-TextView lon;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
        StrictMode.setThreadPolicy(policy);
-        tv = findViewById(R.id.test);
-        lat = findViewById(R.id.lat);
-        lon = findViewById(R.id.lon);
+
 
         GPSTracker gps = new GPSTracker(this);
-        lat.setText(Double.toString(gps.getLatitude()));
-        lon.setText(Double.toString(gps.getLongitude()));
         Weather weather = new Weather();
         weather.getData();
 
         //ThreadService.enqueueWork(this, getIntent());
-
-        //p.start();
-        tv.setText(weather.getPressure());
     }
 
 
